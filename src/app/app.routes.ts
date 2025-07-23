ad9f4486c5e837f8a70c4387a7d969f74557fd1e
Guilherme Felix <f3lixguilherme@gmail.com>
2025-07-23 17:02:02 -0300

    feat(router): adds navegation between pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
+import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: CertificadosComponent
+  },
+  {
+    path: "certificado/novo",
+    component: CertificadoFormComponent
+  },
+  {
+    path: "certificado/:id",
+    component: CertificadoComponent
+  }
+];

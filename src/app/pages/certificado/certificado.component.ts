e5cf09a0fb6ccaf00b1b119c9eb0984d6364a94e
Guilherme Felix <f3lixguilherme@gmail.com>
2025-07-23 16:40:01 -0300

    feat(page): adds page certificate

---
+++ src/app/pages/certificado/certificado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificado',
+  imports: [],
+  templateUrl: './certificado.component.html',
+  styleUrl: './certificado.component.css'
+})
+export class CertificadoComponent {
+
+}
